4b0b934877dc55435f2d21549e57ad8d3fab55ae
Kenar-MC <kenar.fresh@gmail.com>
2025-04-04 18:07:10 +0300

    string

--- L02_strings/src/main.rs
+++ L02_strings/src/main.rs
@@ -1,16 +1,28 @@
 fn main() {
 
-    let hero_name = "Van Damme".to_string();
+    let hero_name = "Van Damme".to_string();        // String
     println!("The hero name is: {:#?}", hero_name);
 
-    let short_nae = hero_name.replace
+    let short_name = hero_name.replace("Van Damme", "JCVD ");   // String
+    println!("The short name is: {:#?}", short_name);
 
-
-    let position = String::from("Quarterback");
+    let position = String::from("Quarterback");     // String
     println!("The position is: {:#?}", position);
 
+    let greetings = "Greetings from the team";   // String
+    println!("The greetings is: {:#?}", greetings);
 
+    let short_greetings = greetings.get(0..9).unwrap();  // &str
+    println!("The short greetings is: {:#?}", short_greetings);
 
+    let hello ="Hello";   // &str
+    let hello_world ="Hello World".to_string();   // String
+    let hello_ref = &hello_world;    // String
+    
+
+    println!("The hello is: {:#?}", hello);
+    println!("The hello world is: {:#?}", hello_world);
+    println!("The hello ref is: {:#?}", hello_ref);
 
 
 }
